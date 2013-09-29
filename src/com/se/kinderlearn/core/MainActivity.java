f7fa05cc4e817ec58c3eca813ac895c056114ea3
John Shelley <john.shelley.p@gmail.com>
2013-09-29 15:08:50 -0500

    refactoring

---
+++ src/com/se/kinderlearn/core/MainActivity.java
@@ -0,0 +1,26 @@
+package com.se.kinderlearn.core;
+
+import com.se.kinderlearn.R;
+import com.se.kinderlearn.R.layout;
+import com.se.kinderlearn.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
