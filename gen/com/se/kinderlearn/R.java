d3498235f8f4ebd05e14c16a9058b30151abae08
John Shelley <john.shelley.p@gmail.com>
2013-10-06 18:18:58 -0500

    Add first equation!

--- gen/com/se/kinderlearn/R.java
+++ gen/com/se/kinderlearn/R.java
@@ -26,12 +26,16 @@ public final class R {
         public static final int planet_earth_in_space=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080009;
         public static final int game_countdown=0x7f080000;
-        public static final int mainWelcomeText=0x7f080004;
-        public static final int main_rules_text=0x7f080002;
-        public static final int main_start_game=0x7f080003;
-        public static final int pager=0x7f080001;
+        public static final int mainWelcomeText=0x7f080008;
+        public static final int main_rules_text=0x7f080006;
+        public static final int main_start_game=0x7f080007;
+        public static final int num_one=0x7f080001;
+        public static final int num_two=0x7f080002;
+        public static final int operator=0x7f080003;
+        public static final int pager=0x7f080005;
+        public static final int user_answer=0x7f080004;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
--- src/com/se/kinderlearn/core/GameActivity.java
+++ src/com/se/kinderlearn/core/GameActivity.java
@@ -1,21 +1,45 @@
 package com.se.kinderlearn.core;
 
-import com.se.kinderlearn.R;
-import com.se.kinderlearn.R.layout;
-import com.se.kinderlearn.R.menu;
+import java.util.Random;
+import java.util.TimerTask;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+
+import com.se.kinderlearn.R;
 
 public class GameActivity extends Activity {
+
+	TextView	textCounter;
+	TextView	numOne;
+	TextView	numTwo;
+	TextView	op;
+	EditText	userAnswer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_game);
+
+		textCounter = (TextView) findViewById(R.id.game_countdown);
+		numOne = (TextView) findViewById(R.id.num_one);
+		numTwo = (TextView) findViewById(R.id.num_two);
+		op = (TextView) findViewById(R.id.operator);
+		userAnswer = (EditText) findViewById(R.id.user_answer);
+
+		MyCount countdown = new MyCount(6000, 1000);
+		countdown.start();
 	}
 
 	@Override
@@ -25,4 +49,76 @@ public class GameActivity extends Activity {
 		return true;
 	}
 
+	// countdowntimer is an abstract class, so extend it and fill in methods
+	public class MyCount extends CountDownTimer {
+
+		public MyCount(long millisInFuture, long countDownInterval) {
+			super(millisInFuture, countDownInterval);
+		}
+
+		@Override
+		public void onFinish() {
+			textCounter.setVisibility(View.INVISIBLE);
+			playGame();
+		}
+
+		@Override
+		public void onTick(long millisUntilFinished) {
+			System.out.println(millisUntilFinished);
+			if (millisUntilFinished >= 5000) {
+				textCounter.setText("Ready?");
+			} else if (millisUntilFinished >= 1000
+					&& millisUntilFinished < 2000) {
+				textCounter.setText("GO!");
+			} else {
+				textCounter.setText("" + (millisUntilFinished - 1000) / 1000);
+			}
+
+		}
+	}
+
+	public void playGame() {
+		Random oneRand = new Random();
+		int one = oneRand.nextInt() % 100;
+		numOne.setText("" + one);
+
+		Random twoRand = new Random();
+		int two = oneRand.nextInt() % 100;
+		numTwo.setText("" + two);
+
+		if (one % 2 == 0) {
+			op.setText("+");
+		} else {
+			op.setText("-");
+		}
+		numOne.setVisibility(View.VISIBLE);
+		numTwo.setVisibility(View.VISIBLE);
+		op.setVisibility(View.VISIBLE);
+		userAnswer.setVisibility(View.VISIBLE);
+		int answer = 0;
+		if (op.getText().equals("+")) {
+			answer = one + two;
+		} else if (op.getText().equals("-")) {
+			answer = one - two;
+		}
+		final int passAnswer = answer;
+		userAnswer.setOnEditorActionListener(new OnEditorActionListener() {
+
+			@Override
+			public boolean onEditorAction(TextView v, int actionId,
+					KeyEvent event) {
+				boolean handled = false;
+				if (actionId == EditorInfo.IME_ACTION_GO) {
+					if (Integer.parseInt(userAnswer.getText().toString()) == passAnswer) {
+						System.out.println("CORRECT");
+					} else {
+						System.out.println("FAILED");
+					}
+					handled = true;
+				}
+				return handled;
+			}
+		});
+
+	}
 }
