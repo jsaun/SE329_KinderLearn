0eb27e6186152bb5fe3b1baaffbf5a5004b13773
John Shelley <john.shelley.p@gmail.com>
2013-10-10 10:15:44 -0500

    Final push for kinderlearn

--- src/com/se/kinderlearn/core/GameActivity.java
+++ src/com/se/kinderlearn/core/GameActivity.java
@@ -1,17 +1,16 @@
 package com.se.kinderlearn.core;
 
 import java.util.Random;
-import java.util.TimerTask;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.os.Handler;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
@@ -25,6 +24,12 @@ public class GameActivity extends Activity {
 	TextView	numTwo;
 	TextView	op;
 	EditText	userAnswer;
+	Button		next;
+
+	int			usersScore;
+	TextView	answerDetail;
+	boolean		waiting;
+	GameThread	game;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +41,13 @@ public class GameActivity extends Activity {
 		numOne = (TextView) findViewById(R.id.num_one);
 		numTwo = (TextView) findViewById(R.id.num_two);
 		op = (TextView) findViewById(R.id.operator);
+		answerDetail = (TextView) findViewById(R.id.answer_detail);
+
 		userAnswer = (EditText) findViewById(R.id.user_answer);
+		next = (Button) findViewById(R.id.new_question);
 
 		MyCount countdown = new MyCount(6000, 1000);
+		usersScore = 0;
 		countdown.start();
 	}
 
@@ -47,6 +56,35 @@ public class GameActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.game, menu);
 		return true;
+	}
+
+	public void nextQuestion(View v) {
+		if (usersScore < 10) {
+			if (game.isAlive()) {
+				Thread kill = game;
+				game = null;
+				kill.interrupt();
+			}
+			game = new GameThread();
+			game.run();
+		} else {
+			setWinStatement();
+		}
+	}
+
+	private void setWinStatement() {
+		this.answerDetail.post(new Runnable() {
+
+			@Override
+			public void run() {
+				answerDetail.setVisibility(View.VISIBLE);
+				answerDetail.setText("You Won!");
+				numOne.setVisibility(View.INVISIBLE);
+				numTwo.setVisibility(View.INVISIBLE);
+				op.setVisibility(View.INVISIBLE);
+
+			}
+		});
 	}
 
 	// countdowntimer is an abstract class, so extend it and fill in methods
@@ -59,7 +97,8 @@ public class GameActivity extends Activity {
 		@Override
 		public void onFinish() {
 			textCounter.setVisibility(View.INVISIBLE);
-			playGame();
+			game = new GameThread();
+			game.run();
 		}
 
 		@Override
@@ -77,48 +116,88 @@ public class GameActivity extends Activity {
 		}
 	}
 
-	public void playGame() {
-		Random oneRand = new Random();
-		int one = oneRand.nextInt() % 100;
-		numOne.setText("" + one);
+	private class GameThread extends Thread {
 
-		Random twoRand = new Random();
-		int two = oneRand.nextInt() % 100;
-		numTwo.setText("" + two);
+		@Override
+		public void run() {
+			waiting = true;
+			Random oneRand = new Random();
+			int one = oneRand.nextInt() % 100;
+			numOne.setText("" + one);
 
-		if (one % 2 == 0) {
-			op.setText("+");
-		} else {
-			op.setText("-");
-		}
-		numOne.setVisibility(View.VISIBLE);
-		numTwo.setVisibility(View.VISIBLE);
-		op.setVisibility(View.VISIBLE);
-		userAnswer.setVisibility(View.VISIBLE);
-		int answer = 0;
-		if (op.getText().equals("+")) {
-			answer = one + two;
-		} else if (op.getText().equals("-")) {
-			answer = one - two;
-		}
-		final int passAnswer = answer;
-		userAnswer.setOnEditorActionListener(new OnEditorActionListener() {
+			Random twoRand = new Random();
+			int two = twoRand.nextInt() % 100;
+			numTwo.setText("" + two);
 
-			@Override
-			public boolean onEditorAction(TextView v, int actionId,
-					KeyEvent event) {
-				boolean handled = false;
-				if (actionId == EditorInfo.IME_ACTION_GO) {
-					if (Integer.parseInt(userAnswer.getText().toString()) == passAnswer) {
-						System.out.println("CORRECT");
-					} else {
-						System.out.println("FAILED");
-					}
-					handled = true;
-				}
-				return handled;
+			if (one % 2 == 0) {
+				op.setText("+");
+			} else {
+				op.setText("-");
 			}
-		});
+			answerDetail.setVisibility(View.INVISIBLE);
+			numOne.setVisibility(View.VISIBLE);
+			numTwo.setVisibility(View.VISIBLE);
+			op.setVisibility(View.VISIBLE);
+			userAnswer.setVisibility(View.VISIBLE);
+			int answer = 0;
+			if (op.getText().equals("+")) {
+				answer = one + two;
+			} else if (op.getText().equals("-")) {
+				answer = one - two;
+			}
+			final int passAnswer = answer;
+			userAnswer.setOnEditorActionListener(new OnEditorActionListener() {
 
+				@Override
+				public boolean onEditorAction(TextView v, int actionId,
+						KeyEvent event) {
+					boolean handled = false;
+					if (actionId == EditorInfo.IME_ACTION_GO) {
+						int guess = 0;
+						try {
+							guess = Integer.parseInt(userAnswer.getText()
+									.toString());
+						} catch (NumberFormatException n) {
+							answerDetail.setVisibility(View.VISIBLE);
+							answerDetail.post(new Runnable() {
+
+								@Override
+								public void run() {
+									answerDetail.setText("Enter a number");
+								}
+							});
+							return false;
+						}
+						if (guess == passAnswer) {
+							answerDetail.setVisibility(View.VISIBLE);
+							answerDetail.post(new Runnable() {
+
+								@Override
+								public void run() {
+									answerDetail.setText("Correct! Score = "
+											+ usersScore);
+								}
+							});
+							usersScore++;
+						} else {
+							answerDetail.setVisibility(View.VISIBLE);
+							answerDetail.post(new Runnable() {
+
+								@Override
+								public void run() {
+									answerDetail.setText("Incorrect! Score = "
+											+ usersScore);
+								}
+							});
+						}
+						handled = true;
+					}
+					userAnswer.setText("");
+					waiting = false;
+					return handled;
+				}
+			});
+
+		}
 	}
 }
