6c3e09c50c8f703e122d9ab3f58b7b7696ab3738
Morgan <morgan.hoke@gmail.com>
2013-11-07 00:05:29 -0600

    I got some graphics working on a background thread

--- src/com/se/kinderlearn/core/SpaceInvadersView.java
+++ src/com/se/kinderlearn/core/SpaceInvadersView.java
@@ -1,8 +1,15 @@
 package com.se.kinderlearn.core;
 
+import com.se.kinderlearn.R;
+
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
@@ -16,8 +23,68 @@ public class SpaceInvadersView extends SurfaceView implements SurfaceHolder.Call
 
 
 	public class SpaceInvadersThread extends Thread{		
+		private Bitmap backgroundImage;
+		private int canvasHeight = 1;
+		private int canvasWidth = 1;
+		private Drawable ship;
+		private Drawable explosion;
+		private Handler mHandler;
+		private int shipHeight;
+		private int shipWidth;
+		private long lastTime;
+		private Paint linePaint;
+		private int mode;
+		private boolean run;
+		private SurfaceHolder mSurfaceHolder;
+		private double X;
+		private double Y;
+		
 		public SpaceInvadersThread(SurfaceHolder surfaceHolder, Context context,
                 Handler handler){
+			mSurfaceHolder = surfaceHolder;
+			mHandler = handler;
+			mContext = context;
+			Resources res = context.getResources();
+			ship = res.getDrawable(R.drawable.ship);
+			explosion = res.getDrawable(R.drawable.explosion);
+			backgroundImage = BitmapFactory.decodeResource(res, R.drawable.planet_earth_in_space);
+			linePaint = new Paint();
+			linePaint.setAntiAlias(true);
+			linePaint.setARGB(255,0,255,0);
+			shipHeight = ship.getIntrinsicHeight();
+			shipWidth = ship.getIntrinsicWidth();
+		}
+		
+		public void doStart(){
+			synchronized(mSurfaceHolder){
+				X = canvasWidth /2;
+				Y = canvasHeight - shipHeight;
+			}
+		}
+		
+		public void run(){
+			while (true) {
+                Canvas c = null;
+                try {
+                    c = mSurfaceHolder.lockCanvas(null);
+                    synchronized (mSurfaceHolder) {
+                    	doDraw(c);
+                    }
+                } finally {
+                    // do this in a finally so that if an exception is thrown
+                    // during the above, we don't leave the Surface in an
+                    // inconsistent state
+                    if (c != null) {
+                        mSurfaceHolder.unlockCanvasAndPost(c);
+                    }
+                }
+            }
+		}
+		
+		public void doDraw(Canvas canvas){
+			canvas.drawBitmap(backgroundImage, 0, 0, null);
+			ship.setBounds(0, 0, shipWidth, shipHeight);
+			ship.draw(canvas);
 		}
 	}
 	
@@ -41,7 +108,6 @@ public class SpaceInvadersView extends SurfaceView implements SurfaceHolder.Call
                 mStatusText.setText(m.getData().getString("text"));
             }
         });
-
         setFocusable(true); // make sure we get key events
 	}
 
@@ -100,11 +166,6 @@ public class SpaceInvadersView extends SurfaceView implements SurfaceHolder.Call
 
 	}
 
-	public void unlockCanvasAndPost(Canvas canvas) {
-		// TODO Auto-generated method stub
-
-	}
-
 	public void surfaceChanged(SurfaceHolder holder, int format, int width,
 			int height) {
 		// TODO Auto-generated method stub
@@ -112,7 +173,8 @@ public class SpaceInvadersView extends SurfaceView implements SurfaceHolder.Call
 	}
 
 	public void surfaceCreated(SurfaceHolder holder) {
-		// TODO Auto-generated method stub
+		thread.start();
+		
 		
 	}
 
