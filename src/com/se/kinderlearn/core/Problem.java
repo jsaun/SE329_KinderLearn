5224f81ce574fadb78c3ca0239127e437a0b09dd
Jonathon <jsaun@iastate.edu>
2013-11-13 15:28:39 -0600

    Add code for possible answers

--- src/com/se/kinderlearn/core/Problem.java
+++ src/com/se/kinderlearn/core/Problem.java
@@ -1,12 +1,54 @@
 package com.se.kinderlearn.core;
 
+import java.util.Random;
+
 public class Problem {
 	private String expression;
 	private int answer;
+	private int[] possibleAnswers;
 	
 	public Problem(String ex, int ans){
 		this.expression = ex;
 		this.answer = ans;
+		this.possibleAnswers = new int[4];
+		Random r = new Random();
+		int pos = r.nextInt(4);
+		int[] fake = new int[3];
+		if(r.nextBoolean()){
+			fake[0] = answer + (r.nextInt(1) + 1);
+		} else {
+			fake[0] = answer - (r.nextInt(1) + 1);
+		}
+		if(r.nextBoolean()){
+			fake[1] = answer + (r.nextInt(4) + 1);
+		} else {
+			fake[1] = answer - (r.nextInt(4) + 1);
+		}		if(r.nextBoolean()){
+			fake[2] = answer + (r.nextInt(9) + 1);
+		} else {
+			fake[2] = answer - (r.nextInt(9) + 1);
+		}
+		if(pos == 0){
+			possibleAnswers[0] = this.answer;
+			possibleAnswers[1] = fake[0];
+			possibleAnswers[2] = fake[1];
+			possibleAnswers[3] = fake[2];
+		} else if(pos == 1){
+			possibleAnswers[1] = this.answer;
+			possibleAnswers[0] = fake[0];
+			possibleAnswers[2] = fake[1];
+			possibleAnswers[3] = fake[2];
+		} else if(pos == 2){
+			possibleAnswers[2] = this.answer;
+			possibleAnswers[1] = fake[0];
+			possibleAnswers[0] = fake[1];
+			possibleAnswers[3] = fake[2];
+		} else {
+			possibleAnswers[3] = this.answer;
+			possibleAnswers[1] = fake[0];
+			possibleAnswers[2] = fake[1];
+			possibleAnswers[0] = fake[2];
+		}
 	}
 
 	public String getExpression() {
@@ -15,6 +57,10 @@ public class Problem {
 
 	public int getAnswer() {
 		return answer;
+	}
+	
+	public int[] getPossibleAnswers(){
+		return possibleAnswers;
 	}
 	
 	public boolean checkAnswer(int ans){
